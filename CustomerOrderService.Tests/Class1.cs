b254db2bc8d1dfea530cae127749bfc04f90a0af
username <username@cognizant.com>
2022-10-19 17:27:54 +0530

    Add project files.

---
+++ CustomerOrderService.Tests/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using System.Configuration;
+using System.ComponentModel.Design;
+using CustomerOrderService;
+
+namespace CustomerOrderService.Tests
+{
+    public class class1
+    {
+        [TestCase]
+        public void When_Gold_20percent()
+        {
+            Customer c = new Customer();
+            c.CustId = 101;
+            c.CustName = "Anee";
+            c.CustomerType = CustomerType.Gold;
+            Order order = new Order();
+            order.OrderId = 1;
+            order.ProductId = 2;
+            order.Qty = 5;
+            order.Amount = 100;
+
+            CustomerOrder co = new CustomerOrder();
+            co.Discount(c, order);
+            Assert.AreEqual(order.Amount, 80);
+
+        }
+        public void Get_customer_list()
+        {
+            SqlConnection cn= new SqlConnection(ConfigurationManager.ConnectionStrings["Mycontext"].ConnectionString);
+            cn.Open();
+            SqlCommand cmd=new SqlCommand("Select * from Customers",cn);
+            List<Customer> custList1 = new List<Customer>();
+
+
+            SqlDataReader dr =cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Customer cust = new Customer();
+                cust.CustName = dr["Name"].ToString();
+                cust.CustId = dr[0].GetHashCode();
+                
+
+            }
+
+
+        }
+    }
+}
---
+++ CustomerOrderService/Customer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOrderService
+{
+    public enum CustomerType
+    {
+        Basic, Premimum, Gold
+    }
+    public class Customer
+    {
+        public int CustId { get; set; }
+
+        public string CustName { get; set; }
+
+        public CustomerType CustomerType { get; set; }
+
+    }
+
+    public class Order
+    {
+        public int OrderId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Qty { get; set; }
+
+        public double Amount { get; set; }
+    }
+
+    public class CustomerOrder
+    {
+        public void Discount(Customer c, Order o)
+        {
+            if (c.CustomerType == CustomerType.Premimum)
+            {
+                o.Amount = o.Amount - (o.Amount * 10 / 100);
+            }
+            else if (c.CustomerType == CustomerType.Gold)
+            {
+                o.Amount = o.Amount - (o.Amount * 20 / 100);
+            }
+        }
+    }
+}
